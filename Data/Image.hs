98f375f1882137a5592e57eaaa4e958a128a4e0b
jcollard <jcollard@unm.edu>
2013-05-06 11:12:06 -0600

    Added reference to README.md in the haddock for getting started.

--- Data/Image.hs
+++ Data/Image.hs
@@ -1,5 +1,9 @@
 {-| The Univeristy of New Mexico's Haskell Image Processing library contains
     functionality for performing manipulation gray, color, and complex images.
+
+    If you want to jump right in, simply import this module and view the examples at the following URL:
+
+    <https://github.com/jcollard/unm-hip>
 -}
 module Data.Image(
   -- | By importing Data.Image you will get functionality for working with Boxed images
